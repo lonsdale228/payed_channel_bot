9f7c4030f8d2c629502f5ee8fc6317a1ae323f87
bebus <v.gerashchenkoua@gmail.com>
2025-04-14 19:37:14 +0300

    Added main features, setup logger

--- loader.py
+++ loader.py
@@ -3,10 +3,13 @@ import os
 
 from aiogram import Bot, Dispatcher
 
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 dp = Dispatcher()
 bot = Bot(token=BOT_TOKEN)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+
